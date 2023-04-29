9fe8ca5e5dd05ce8bf74f113184ad52d2b589220
MishaHudz <misha.hudz@gmail.com>
2023-04-29 20:52:41 +0300

    finish task one and two

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const listLink = document.querySelector('.gallery');
+
+function createGalary(photosInformArr) {
+  return photosInformArr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+listLink.insertAdjacentHTML('afterbegin', createGalary(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const watchProgres = Number.parseFloat(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+if (!watchProgres) localStorage.setItem('videoplayer-current-time', '0');
+
+player.setCurrentTime(watchProgres);
+
+player.on('timeupdate', throttle(timeSet, 1000));
+
+function timeSet(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+}
